2ef4163a7b50772adfcfc56776567a12d1ed64b0
thempel <thempel@zedat.fu-berlin.de>
2018-06-18 09:51:16 +0200

    simplify crossover channel tests

--- information/tests/test_crossoverchannel.py
+++ information/tests/test_crossoverchannel.py
@@ -4,60 +4,45 @@ import numpy as np
 import msmtools
 
 
+# true output
+def entropy1D(x):
+    return - x * np.log2(x) - (1 - x) * np.log2(1 - x)
+
+
 class TestCrossover(unittest.TestCase):
-    def test_MSMInfo(self):
+
+    @classmethod
+    def setUpClass(cls):
         p = 0.3
         eps = .2
         N = int(1e4)
         T = np.array([[1 - p, p], [p, 1 - p]])
-        X = msmtools.generation.generate_traj(T, N)
+        cls.X = msmtools.generation.generate_traj(T, N)
         _errbits = np.random.rand(N) < eps
-        Y = X.copy()
-        Y[_errbits] = 1 - Y[_errbits]
+        cls.Y = cls.X.copy()
+        cls.Y[_errbits] = 1 - cls.Y[_errbits]
 
-        # true output
-        def entropy1D(x):
-            # the other thing seems to be for CTW specifically, taking into account negated x (?)
-            #return - x * np.log2(x)
-            return - x * np.log2(x) - (1 - x) * np.log2(1 - x)
-
-        true_value_DI = entropy1D(p) - (((1 - p) * (1 - eps) + p * eps) * entropy1D(p * eps / ((1 - p) * (1 - eps) + p * eps)) + \
+        cls.true_value_DI = entropy1D(p) - (((1 - p) * (1 - eps) + p * eps) * entropy1D(p * eps / ((1 - p) * (1 - eps) + p * eps)) + \
                     ((p) * (1 - eps) + (1 - p) * eps) * entropy1D((1 - p) * eps / ((p) * (1 - eps) + (1 - p) * eps)))
 
 
-        prob = information.MSMProbabilities().estimate(X, Y)
+    def test_MSMInfo(self):
+
+        prob = information.MSMProbabilities().estimate(self.X, self.Y)
         estimator = information.JiaoI4(prob)
-        estimator.estimate(X, Y)
+        estimator.estimate(self.X, self.Y)
 
         self.assertAlmostEqual(estimator.d + estimator.r, estimator.m)
-        self.assertAlmostEqual(estimator.d, true_value_DI, places=1)
-        self.assertLess(estimator.r, .1 * true_value_DI)
+        self.assertAlmostEqual(estimator.d, self.true_value_DI, places=1)
+        self.assertLess(estimator.r, .1 * self.true_value_DI)
 
 
     def test_CTWInfo(self):
-        p = 0.3
-        eps = .2
-        N = int(1e4)
-        T = np.array([[1 - p, p], [p, 1 - p]])
-        X = msmtools.generation.generate_traj(T, N)
-        _errbits = np.random.rand(N) < eps
-        Y = X.copy()
-        Y[_errbits] = 1 - Y[_errbits]
 
-        # true output
-        def entropy1D(x):
-            # the other thing seems to be for CTW specifically, taking into account negated x (?)
-            #return - x * np.log2(x)
-            return - x * np.log2(x) - (1 - x) * np.log2(1 - x)
-
-        true_value_DI = entropy1D(p) - (((1 - p) * (1 - eps) + p * eps) * entropy1D(p * eps / ((1 - p) * (1 - eps) + p * eps)) + \
-                    ((p) * (1 - eps) + (1 - p) * eps) * entropy1D((1 - p) * eps / ((p) * (1 - eps) + (1 - p) * eps)))
-
-
-        prob = information.CTWProbabilities(5).estimate(X, Y)
+        prob = information.CTWProbabilities(5).estimate(self.X, self.Y)
         estimator = information.JiaoI4(prob)
-        estimator.estimate(X, Y)
+        estimator.estimate(self.X, self.Y)
 
         self.assertAlmostEqual(estimator.d + estimator.r, estimator.m)
-        self.assertAlmostEqual(estimator.d, true_value_DI, places=1)
-        self.assertLess(estimator.r, .1 * true_value_DI)
+        self.assertAlmostEqual(estimator.d, self.true_value_DI, places=1)
+        self.assertLess(estimator.r, .1 * self.true_value_DI)
