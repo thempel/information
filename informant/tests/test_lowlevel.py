15be9a6031d6250cba9c03384037d46c5ac23e52
thempel <thempel@zedat.fu-berlin.de>
2019-08-21 14:27:38 +0200

    adjust unittests

--- informant/tests/test_lowlevel.py
+++ informant/tests/test_lowlevel.py
@@ -1,7 +1,7 @@
 import unittest
-import informant
 import numpy as np
 import itertools
+from informant.utils import multivariate_mutual_info
 
 
 class TestLowlevel(unittest.TestCase):
@@ -11,46 +11,39 @@ class TestLowlevel(unittest.TestCase):
 
     def test_multivariate_mi_independent(self):
 
-        class P:
-            def __init__(self):
-                self.pi_x = [.5, .5]
-                self.pi_y = [.3, .7]
-                self.pi_w = [.9, .1]
 
-                self.pi_xy = [p1 * p2 for p1, p2 in itertools.product(self.pi_y, self.pi_x)]
-                self.pi_xw = [p1 * p2 for p1, p2 in itertools.product(self.pi_w, self.pi_x)]
-                self.pi_yw = [p1 * p2 for p1, p2 in itertools.product(self.pi_w, self.pi_y)]
+        pi_x = [.5, .5]
+        pi_y = [.3, .7]
+        pi_w = [.9, .1]
 
-                self.pi_xyw = []
-                for raveled_index in range(2**3):
-                    n, m, k = np.unravel_index(raveled_index, (2, 2, 2))
-                    self.pi_xyw.append(self.pi_x[n] * self.pi_y[m] * self.pi_w[k])
+        pi_xy = [p1 * p2 for p1, p2 in itertools.product(pi_y, pi_x)]
+        pi_xw = [p1 * p2 for p1, p2 in itertools.product(pi_w, pi_x)]
+        pi_yw = [p1 * p2 for p1, p2 in itertools.product(pi_w, pi_y)]
 
-        est = informant.CausallyConditionedDI(P())
-        est.Nx = est.Ny = est.Nw = 2  # manually set properties
-        m = est._multivariate_mutual_info()
+        pi_xyw = []
+        for raveled_index in range(2**3):
+            n, m, k = np.unravel_index(raveled_index, (2, 2, 2))
+            pi_xyw.append(pi_x[n] * pi_y[m] * pi_w[k])
+
+        m = multivariate_mutual_info(pi_x, pi_y, pi_w, pi_xy, pi_xw, pi_yw, pi_xyw)
 
         self.assertAlmostEqual(m, 0.)
 
     def test_multivariate_mi_dependent(self):
 
-        class P:
-            def __init__(self):
-                self.pi_x = [.5, .5]
-                self.pi_y = [.3, .7]
-                self.pi_w = [.9, .1]
+        pi_x = [.5, .5]
+        pi_y = [.3, .7]
+        pi_w = [.9, .1]
 
-                self.pi_xy = self.pi_xw = self.pi_yw = np.ones(4)/4
-                self.pi_xyw = np.ones(8)/8
-        prob = P()
-        est = informant.CausallyConditionedDI(prob)
-        est.Nx = est.Ny = est.Nw = 2  # manually set properties
-        m = est._multivariate_mutual_info()
+        pi_xy = pi_xw = pi_yw = np.ones(4)/4
+        pi_xyw = np.ones(8)/8
+
+        m = multivariate_mutual_info(pi_x, pi_y, pi_w, pi_xy, pi_xw, pi_yw, pi_xyw)
 
         def summand(p1, p2, p3):
             return 1/8 * np.log2(.25**3 / (p1 * p2 * p3 * 1/8))
 
         m_test = sum([summand(p1, p2, p3) for p1, p2, p3 in
-                      itertools.product(prob.pi_x, prob.pi_y, prob.pi_w)])
+                      itertools.product(pi_x, pi_y, pi_w)])
 
         self.assertAlmostEqual(m, m_test)
